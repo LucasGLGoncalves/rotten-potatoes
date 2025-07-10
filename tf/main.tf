67929d74e6d3dc5f04615fa7c31f6f97e9fbd471
LucasGLGoncalves <lucas.gabrielsglgl@gmail.com>
2025-07-10 15:12:50 -0400

    ec2 file

--- tf/main.tf
+++ tf/main.tf
@@ -10,27 +10,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-
-
-
-
-# EC2 pública (aplicação)
-resource "aws_instance" "app" {
-  ami             = "ami-0c02fb55956c7d316" # Amazon Linux 2
-  instance_type   = "t2.micro"
-  subnet_id       = aws_subnet.public.id
-  key_name        = "minha-chave-ssh"
-  security_groups = [aws_security_group.app_sg.id]
-  tags            = { Name = "app-server" }
-}
-
-# EC2 privada (banco de dados)
-resource "aws_instance" "db" {
-  ami             = "ami-0c02fb55956c7d316"
-  instance_type   = "t2.micro"
-  subnet_id       = aws_subnet.private.id
-  key_name        = "minha-chave-ssh"
-  security_groups = [aws_security_group.db_sg.id]
-  tags            = { Name = "db-server" }
-}
